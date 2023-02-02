425dca80a86bdbc7b738c80ff633f730398903fc
keirastanley <keiraestanley@gmail.com>
2023-02-02 18:09:31 +0000

    Next app initialised, default content removed

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cyber-Mix</title>
+        <meta name="description" content="Mixtape-making in the digital age" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/cyber-mix-logo.png" />
+      </Head>
+    </>
+  )
+}
