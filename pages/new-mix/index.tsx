d2faf68239ec7f4971f852216c6e0d164efc7159
keirastanley <keiraestanley@gmail.com>
2023-02-02 22:15:27 +0000

    Make playlist page and basic functionality built

--- pages/new-mix/index.tsx
+++ pages/new-mix/index.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function NewMix(){
-    return <div>Make a playlist</div>
+
+    const [playlistSettings, setPlaylistSettings] = useState<{[key: string]: string}>()
+
+    function getPlaylistSettings(event: { target: { name: string; value: string } }){
+        const newPlaylistSettings = {...playlistSettings}
+        newPlaylistSettings[event.target.name] = event.target.value
+        setPlaylistSettings(newPlaylistSettings)
+    }
+
+    return <div>
+        <h1>Make a playlist</h1>
+        <p>Name</p>
+        <input onBlur={getPlaylistSettings} name="name"></input>
+        <p>Description</p>
+        <input onBlur={getPlaylistSettings} name="description"></input>
+        <p>Select a setting</p>
+        <select onChange={getPlaylistSettings} name="setting">
+            <option>Public</option>
+            <option>Private</option>
+        </select>
+</div>
 }
