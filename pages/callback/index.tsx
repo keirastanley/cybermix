fca9aa58972f3a75e77fd1c832cdbe3b00886a98
keirastanley <keiraestanley@gmail.com>
2023-02-03 20:03:47 +0000

    User moved to app from callback

--- pages/callback/index.tsx
+++ pages/callback/index.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 
 export default function Callback() {
 
-    const [user, setUser] = useState<string>()
     const [token, setToken] = useState<string>()
     const router = useRouter()
   
