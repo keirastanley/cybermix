d12312344b16ef227a883fa7c5619350447138a1
keirastanley <keiraestanley@gmail.com>
2023-02-09 20:56:16 +0000

    Callback path added - seems to be necessary when site is deployed

---
+++ pages/callback/index.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Callback() {
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push("/")
+    }, [])
+}
